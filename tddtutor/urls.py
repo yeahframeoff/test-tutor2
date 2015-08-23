676c6124c86ab66a3c6637dc643bb20dfc8b6e26
yeahframeoff <anthyef@gmail.com>
2015-08-23 21:24:03 +0300

    new URL  + view for adding new items to existing lists. FT finally passes!

--- tddtutor/urls.py
+++ tddtutor/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', listviews.home_page, name='home'),
     url(r'^lists/new$', listviews.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', listviews.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', listviews.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', listviews.add_item, name='add_item'),
 ]
