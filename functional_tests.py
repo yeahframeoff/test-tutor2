f7403563a1b4fbeda1cab6c6e0fedd20ee7923b3
yeahframeoff <anthyef@gmail.com>
2015-08-23 14:11:02 +0300

    Function test refactor

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage.
@@ -36,9 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # lists "#1: Fry potatoes" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('#1: Fry potatoes', [row.text for row in rows])
+        self.check_for_row_in_list_table('#1: Fry potatoes')
 
         # There is still a textbox inviting her to add another item.
         # She enters "Bake some bread"
@@ -47,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('#1: Fry potatoes', [row.text for row in rows])
-        self.assertIn('#2: Bake some bread', [row.text for row in rows])
+        self.check_for_row_in_list_table('#1: Fry potatoes')
+        self.check_for_row_in_list_table('#2: Bake some bread')
 
         self.fail('Finish the test!')
 
